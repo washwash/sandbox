cfeac928ad208ce14936943417605beeaa2b56f4
washwash <gordeev.and.and@gmail.com>
2022-01-18 01:18:43 +0100

    Update kitchen.py

--- kitchen.py
+++ kitchen.py
@@ -31,19 +31,73 @@ class Product:
 
 class Shelf:
     product: Product
-    measuremen: Measurmen
+    measurement: Measurment
 
     def __str__(self):
         return f'{self.product} ({self.measurment})'
 
 class Ingredient:
     shelf: Shelf
-    measuremen: Measurmen
+    measurement: Measurment
 
     def __str__(self):
         return f'{self.shelf} ({self.measurment})'
+
+    #def bind(self, ):
 
 class Recipe:
     ingredients: List[Ingredient]
     description: str
     name: str
+
+    def __init__(self, name, ingredients, description=None):
+        self.name = name
+        self.ingredients = ingredients
+        self.description = description
+
+    def bind_ingredients(self):
+        for ingredient in self.ingredients:
+            ingredient.bind(self)
+
+## Client code
+# define abstract users products
+penne = Product("penne")
+spaghetti = Product("spaghetti")
+pesto_shop = Product("Pesto from shop")
+
+# add some value to abstract products and make them tangible
+penne_shelf = Shelf(
+    penne,
+    Measurment(measurement=MeasurmentUnit.GRAM.value, quantity=Decimal(1000))
+)
+
+spaghetti_shelf = Shelf(
+    spaghetti,
+    Measurment(measurement=MeasurmentUnit.GRAM.value, quantity=Decimal(200))
+)
+
+pesto_shop_shelf = Shelf(
+    pesto_shop,
+    Measurment(measurement=MeasurmentUnit.PIECE.value, quantity=Decimal(1))
+)
+
+# add actual products in the kitchen to some recepies and dishes
+pesto_homemade = Recipe("Pesto home made")
+
+ingredients = [
+    Ingredient(
+        shelf=pesto_shop_shelf,
+        measurment=Measurment(
+            measurement=MeasurmentUnit.PIECE.value,
+            quantity=Decimal(1)
+        )
+    ),
+    Ingredient(
+        shelf=penne_shelf,
+        measurment=Measurment(
+            measurement=MeasurmentUnit.GRAM.value,
+            quantity=Decimal(250)
+        )
+    )
+]
+pasta_pesto_dish = Recipe("Pasta pesto shop", ingredients=ingredients)
